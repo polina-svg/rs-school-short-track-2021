683160cd8603c1e3d84b356d031f62de90f4d0e3
polina-svg <pshibanova000@gmail.com>
2021-04-11 23:44:47 +0300

    feat: file names - done

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,20 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const result = [];
+  let suffix = 1;
+  for (let i = 0; i < names.length; i++) {
+    if (!result.includes(names[i])) {
+      result.push(names[i]);
+    } else if (/\w*\(\d\)/.test(names[i])) {
+      result.push(`${names[i]}(${suffix - 1})`);
+    } else {
+      result.push(`${names[i]}(${suffix})`);
+      suffix++;
+    }
+  }
+  return result;
 }
 
 module.exports = renameFiles;
