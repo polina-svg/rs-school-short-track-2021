0c8e20632f5bf955410db8d5faf5dbb407dd8653
polina-svg <pshibanova000@gmail.com>
2021-04-12 14:07:26 +0300

    feat: find index solved

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,15 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let counter = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      counter = array.indexOf(value);
+      break;
+    }
+  }
+  return counter;
 }
 
 module.exports = findIndex;
