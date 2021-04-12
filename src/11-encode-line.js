95622070c9f1769fb90579aaf56a1becb07a03a0
polina-svg <pshibanova000@gmail.com>
2021-04-12 15:02:01 +0300

    feat: add encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(string) {
+  let counter = 0;
+  const str = string.split('');
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    counter++;
+    if (counter === 1 && str[i] !== str[i + 1]) {
+      result += str[i];
+      counter = 0;
+    }
+    if (counter > 1 && str[i + 1] !== str[i]) {
+      result = result + counter + str[i];
+      counter = 0;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
